e7de9df3d0f8a042780aeff6d114eae4ef38da74
Legenderox <brain1127@outlook.com>
2024-08-14 01:00:03 +0200

    Upploaded all Local files

---
+++ util/data_visualization.py
@@ -0,0 +1,62 @@
+"""
+This script visualizes a set of images stored in a NumPy array using matplotlib.
+The script allows the user to navigate through the images using Next and Previous buttons,
+and also provides an input box to directly jump to a specific image index.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from matplotlib.widgets import Button, TextBox
+
+current_path = Path("C:/Users/brain/My Drive (brain@roforco.com)/CodeInDocs/MyDiffusionModel/tests/data_visualization.py").resolve()
+
+# Change the path to the model you want to visualize
+# data = np.load(current_path.parent.parent / "data" / "sprites_1788_16x16.npy")
+data = np.load(current_path.parent.parent / "generations" / "custom_modelV4.npy")
+
+fig, axes = plt.subplots(8,8, figsize=(15,15))
+plt.subplots_adjust(bottom=0.1, hspace=0.4)
+
+current_index = 0
+for i, ax in enumerate(axes.flat):
+    ax.imshow(data[i])
+    current_index = i
+    ax.set_title(f"Image {current_index}")
+
+def next_images(event):
+    global current_index
+    for i, ax in enumerate(axes.flat):
+        current_index += 1
+        ax.imshow(data[current_index])
+        ax.set_title(f"Image {current_index}")
+    plt.title(f"Showing Images: {current_index - len(axes.flat)} - {current_index}")
+    plt.draw()
+    
+def prev_images(event):
+    global current_index
+    current_index = max(-1, current_index - len(axes.flat)*2)
+    next_images(event)
+
+def update_index(text):
+    global current_index
+    current_index = int(text) - 1
+    next_images(None)
+
+# Create button axes
+next_ax = plt.axes([0.81, 0.05, 0.1, 0.03])
+prev_ax = plt.axes([0.7, 0.05, 0.1, 0.03])  # [left, bottom, width, height]
+input_ax = plt.axes([0.1, 0.05, 0.5, 0.03])
+
+# Create buttons
+btn_next = Button(next_ax, 'Next')
+btn_prev = Button(prev_ax, 'Previous')
+text_box = TextBox(input_ax, 'Index:', initial=f"{current_index}")
+
+# Connect buttons to callback functions
+btn_next.on_clicked(next_images)
+btn_prev.on_clicked(prev_images)
+text_box.on_submit(update_index)
+
+plt.title(f"Showing Images: {current_index - len(axes.flat)} - {current_index}")
+plt.show()
---
+++ util/download_images.py
@@ -0,0 +1,53 @@
+"""
+This script downloads and processes images based on the specified parameters.
+
+Functions:
+- resize_image: Resizes the input image to a specified size.
+- add_padding: Adds padding to the input image.
+"""
+
+import numpy as np
+from pathlib import Path
+from PIL import Image
+
+current_path = Path("C:/Users/brain/My Drive (brain@roforco.com)/CodeInDocs/MyDiffusionModel/tests/data_visualization.py").resolve()
+
+# Change the path to the images you want to download
+data = np.load(current_path.parent.parent / "generations" / "custom_modelV4.npy")
+
+# Specify specific images to download (0 index), leave empty to download all images
+images_to_download = [89, 141, 149, 234, 317, 366, 665]
+
+# Change the path to the folder where you want to save the images
+save_path = current_path.parent.parent / "images"
+OUTPUT_SIZE = 462 # square image
+PADDING = 50 # Padding width
+
+def resize_image(image, h, w, original = 16):
+    tiles_per_row = w // original
+    tiles_per_col = h // original
+
+    # Tiling the image
+    image_512x512 = np.repeat(np.repeat(image, tiles_per_col, axis=0), tiles_per_row, axis=1)
+    return image_512x512
+
+def add_padding(image, padding_width = 10, color = 255):
+    padded_image = np.pad(image, 
+                        pad_width=((padding_width, padding_width), 
+                                    (padding_width, padding_width), 
+                                    (0, 0)), 
+                        mode='constant', 
+                        constant_values=color)
+    return padded_image
+
+if __name__ == "__main__":
+    for i, img in enumerate(data):
+        if not images_to_download or (i) in images_to_download:
+            if img.dtype != np.uint8:
+                img = (img * 255).astype(np.uint8)
+
+            img = resize_image(img, OUTPUT_SIZE, OUTPUT_SIZE)
+            img = add_padding(img, padding_width=PADDING)
+            
+            image = Image.fromarray(img)
+            image.save(save_path / f'image{i}.png')
